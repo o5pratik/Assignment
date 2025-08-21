3d188cb53c8837ba0c2a7c3d235d83f8f1dbda82
o5pratik <raj2007varun@gmail.com>
2025-08-21 21:26:17 +0530

    Add files via upload

---
+++ Assignment 1(Task 1).py
@@ -0,0 +1,10 @@
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+add=x+y
+subt=x-y
+multi=x*y
+divi=x/y
+print("Addistion:",add)
+print("Subtract:",subt)
+print("Multiplication:",multi)
+print("Division:",divi)
---
+++ Assignment 1(Task 2).py
@@ -0,0 +1,4 @@
+x=input("Enter your first name:")
+y=input("Enter your last name:")
+
+print(f"Hello, {x} {y}! Welcome to the Python program.")
